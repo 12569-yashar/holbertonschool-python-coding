ff29ac313731f115932c94742f6d98279ed1f1e9
Your Name <you@example.com>
2025-11-20 15:31:40 +0400

    salam

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Defines a Square class."""
 
 class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        """Initialize the square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the square.
 
         Raises:
             TypeError: If size is not an integer.
-            ValueError: If size is < 0.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
